aba55a8fa0d6ffba0a8f70fa964a2a5554b1d0d0
oihv <kokolas16@gmail.com>
2025-09-02 12:52:16 +0800

    Integrate libwebsockets to the project

---
+++ frontend/network/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nikolas/Programming/testing/libwebsockets/minimal-examples-lowlevel/ws-client/minimal-ws-client
+# Build directory: /home/nikolas/Programming/testing/libwebsockets/minimal-examples-lowlevel/ws-client/minimal-ws-client
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
