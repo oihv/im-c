991982e7b6eae45c3167fbfc3344a192c6312b20
oihv <kokolas16@gmail.com>
2025-09-18 18:07:25 +0800

    Backend cross compilation

---
+++ backend/build-windows/CMakeFiles/lws-minimal-ws-server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lws-minimal-ws-server.dir/minimal-ws-server.c.obj"
+  "CMakeFiles/lws-minimal-ws-server.dir/minimal-ws-server.c.obj.d"
+  "liblws-minimal-ws-server.dll.a"
+  "lws-minimal-ws-server.exe"
+  "lws-minimal-ws-server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lws-minimal-ws-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
